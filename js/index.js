f3852efa847ce323c06e3d40b818faf4cfc56645
RebeccaTim <rebecca.timahieu@outlook.fr>
2017-03-30 19:02:54 +0200

    modif affichage ville

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 $('#login-button').click(function(){
   $('#login-button').fadeOut("slow",function(){
-    $("#container").fadeIn();
-    TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
-    TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+    $("#container2").fadeIn();
+    TweenMax.from("#container2", .4, { scale: 0, ease:Sine.easeInOut});
+    TweenMax.to("#container2", .4, { scale: 1, ease:Sine.easeInOut});
   });
 });
 
 $(".close-btn").click(function(){
-  TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
-  TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
-  $("#container, #forgotten-container").fadeOut(800, function(){
+  TweenMax.from("#container2", .4, { scale: 1, ease:Sine.easeInOut});
+  TweenMax.to("#container2", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+  $("#container2, #forgotten-container").fadeOut(800, function(){
     $("#login-button").fadeIn(800);
   });
 });
 
 /* Villes précédentes */
 $('#forgotten').click(function(){
-  $("#container").fadeOut(function(){
+  $("#container2").fadeOut(function(){
     $("#forgotten-container").fadeIn();
   });
 });
