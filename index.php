49ac16f03412e4d55966c5c57cfbf570b852849c
Antoine <antoine.thomire@gmail.com>
2017-03-30 21:30:58 +0200

    Systeme ville bdd ajoute

--- index.php
+++ index.php
@@ -38,18 +38,37 @@
     </div>
   </form>
 </div>
-<!-- Trouver une ville -->
+<!-- Villes précédentes -->
 <div id="forgotten-container">
- <h1>Bi-bi-bite</h1>
+ <h1>Villes précédentes</h1>
  <span class="close-btn">
   <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"></img>
 </span>
-
 <form>
-  <input type="bi-bite" name="bi-bite" placeholder="bi-bite">
-  <a href="#" class="orange-btn">bite</a>
+  <script type="text/javascript">// <![CDATA[
+function deroule(champ,valeur)
+{/valeur est la hauteur en pixel de la zone/
+switch (champ)
+{
+case 2: /si champ vaut 2 alors on change la hauteur de zone2/
+    document.getElementById("zone2").style.height=valeur+'px';
+    break;
+}
+}
+// ]]></script>
+<div id="zone2" style="width: 100%; height: 20px; background: White; border: 1px solid DimGrey; transition: height 1s; -moz-transition: height 1s;-webkit-transition: height 1s;-o-transition: height 1s; overflow: hidden;">
+    <div id="bandeau2" style="text-align: center; height: 20px; width: 100%; font-size: medium; color: white; background-color: darkgrey;" onmouseover="deroule(2,250);" onmouseout="deroule(2,20);">Villes Précédentes
+    </div>
+    <div id="texte2" style="text-align: center;">Antartica
+    </div>
+</div>
+  <a href="#" class="orange-btn">Selectionner</a>
 </form>
 </div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.1/TweenMax.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+<script src="js/index.js"></script>
 <script>
   (function(d, s, id) { 
   var js, djs = d.getElementsByTagName(s)[0];
